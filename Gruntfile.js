9f48a6f8f1a429db55782f761535a285a14f8c39
Andy Mantell <contact@andymantell.com>
2013-04-15 19:18:28 +0100

    Set grunt up to compile the scss

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+// Grunt.js config
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Project configuration.
+  grunt.initConfig({
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+
+    watch: {
+      scss: {
+        files: 'public/ui/**/*.scss',
+        tasks: ['compass']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', []);
+
+  grunt.registerTask('scss', ['compass']);
+
+};
---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "public/ui/css"
+sass_dir = "public/ui/scss"
+images_dir = "public/ui/img"
+javascripts_dir = "public/ui/js"
+
+output_style = :compressed
+
+
+# Remove SASS/Compass relative comments.
+line_comments = false
