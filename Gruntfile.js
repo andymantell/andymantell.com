14771f6477a89e3e65316f08b201fe566b2ec3a3
Andy Mantell <contact@andymantell.com>
2013-07-24 11:13:51 +0100

    Initial setup of Jekyll and corresponding grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-jekyll');
 
   // Project configuration.
   grunt.initConfig({
@@ -19,12 +20,26 @@ module.exports = function(grunt) {
       scss: {
         files: 'src/assets/**/*.scss',
         tasks: ['compass']
+      },
+
+      build: {
+        files: [
+          'src/**/*.*',
+          '!src/assets/**/*.*'
+        ],
+        tasks: ['jekyll', 'compass']
+      }
+    },
+
+    jekyll: {
+      dist: {
+        config: '_config.yml'
       }
     }
   });
 
   // Default task.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jekyll', 'compass']);
 
   grunt.registerTask('scss', ['compass']);
 
