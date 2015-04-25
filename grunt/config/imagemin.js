92c95cb85ad0cbde2ae291cea52e2996b5ba602e
Andy Mantell <contact@andymantell.com>
2015-04-25 17:00:30 +0100

    Imagemin

---
+++ grunt/config/imagemin.js
@@ -0,0 +1,27 @@
+var mozjpeg = require('imagemin-jpegtran');
+var pngquant = require('imagemin-pngquant');
+var gifsicle = require('imagemin-gifsicle');
+var svgo = require('imagemin-svgo');
+
+module.exports = {
+  dist: {
+    options: {
+      optimizationLevel: 5,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [
+        mozjpeg(),
+        svgo(),
+        pngquant(),
+        gifsicle()
+      ]
+    },
+    files: [{
+      expand: true,
+      cwd: 'images/',
+      src: ['**/*.{png,jpg,jpeg,gif,svg}'],
+      dest: 'dist/images/'
+    }]
+  }
+};
