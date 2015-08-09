993806c3f1271b0af5e773b56644b42c91eefce4
Andy Mantell <contact@andymantell.com>
2015-08-09 16:30:22 +0100

    Copy PDF generated version of CV to dist

--- grunt/tasks/get_cv.js
+++ grunt/tasks/get_cv.js
@@ -1,14 +1,19 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var Promise = require('es6-promise').Promise;
 var cv = require('cv');
 
 module.exports = function(grunt) {
   grunt.registerTask('get_cv', 'Get CV', function () {
-    var done = this.async;
+    var done = this.async();
 
     Promise.all([cv.html, cv.pdf])
       .then(function(results) {
-        console.log(results);
+
+        // results[0]
+
+        fs.copySync(results[1], 'dist/CV.pdf');
+
+        done();
       });
 
   });
