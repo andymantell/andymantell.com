fef8fbda42dca989b56976f83b1a5ee289f41a43
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-05-09 13:59:12 +0100

    Minify images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const imagemin = require('gulp-imagemin')
 
 module.exports = (gulp, config) => {
   gulp.task('appImages', () =>
     gulp
       .src(path.join(config.sourcePath, 'images/**'))
+      .pipe(imagemin())
       .pipe(gulp.dest(path.join(config.destinationPath, 'images/app')))
   )
 
